691cfc780205112ed3fa2b396a01ad60be72d874
Rajinder Kaur <105703846+Rajinderkaur86@users.noreply.github.com>
2023-12-16 04:18:29 -0500

    updated js file
    changes made in button.addEventListener

--- index.js
+++ index.js
@@ -12,27 +12,25 @@ let massEl = document.getElementById("mass-el");
 const meterToFeet = 3.281;
 const literToGallon = 0.264;
 const kiloToPound = 2.204;
-const feetToMeter = 0.305;
-const gallonToLiter = 3.785;
-const poundToKilo = 0.45;
+
 button.addEventListener("click", function () {
   let baseValue = inputEl.value;
 
   lengthEl.textContent = `${baseValue} meters = ${(
     baseValue * meterToFeet
-  ).toFixed(2)} feet | ${baseValue} feet = ${(baseValue * feetToMeter).toFixed(
+  ).toFixed(2)} feet | ${baseValue} feet = ${(baseValue / meterToFeet).toFixed(
     2
   )} meters`;
 
   volumeEl.textContent = `${baseValue} liters = ${(
     baseValue * literToGallon
   ).toFixed(2)} gallons | ${baseValue} gallons = ${(
-    baseValue * gallonToLiter
+    baseValue / literToGallon
   ).toFixed(2)} liters`;
 
   massEl.textContent = `${baseValue} kilos = ${(
     baseValue * kiloToPound
   ).toFixed(2)} pounds | ${baseValue} pounds = ${(
-    baseValue * poundToKilo
+    baseValue / kiloToPound
   ).toFixed(2)} kilos`;
 });
